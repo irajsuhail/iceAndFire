4372016e70f485d562be914f8f739f5e2ec27ce9
Raj Suhail <rajsuhailmalik@gmail.com>
2020-09-17 12:43:38 +0530

    Updates

---
+++ precache-manifest.414bbcbc4b619eb768ae32accf4a3bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885ae101f752b26d78100a2e746597f7",
+    "url": "/iceAndFire/index.html"
+  },
+  {
+    "revision": "ea73dbbfae41cf340d7b",
+    "url": "/iceAndFire/static/css/main.62dc3ad4.chunk.css"
+  },
+  {
+    "revision": "4d35021b1e5dc332dbe4",
+    "url": "/iceAndFire/static/js/2.150ba076.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iceAndFire/static/js/2.150ba076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea73dbbfae41cf340d7b",
+    "url": "/iceAndFire/static/js/main.e2601cb4.chunk.js"
+  },
+  {
+    "revision": "3bd17cf8f019fe60a13b",
+    "url": "/iceAndFire/static/js/runtime-main.1b5a466f.js"
+  }
+]);
